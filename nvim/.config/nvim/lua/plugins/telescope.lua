2007ea0fe0f16fa82ee1186eda9f2692dcff2d08
Mac Deluca <Mac.Deluca@quartech.com>
2024-06-06 16:37:37 -0700

    feat: removed nvim-webdev-icons package from neovim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
       cond = function() return vim.fn.executable('make') == 1 end,
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    --{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
     require('telescope').setup({
@@ -36,6 +36,7 @@ return {
         find_files = {
           file_ignore_patterns = { 'node_modules', '.git', 'package-lock.json' },
           hidden = true,
+          no_ignore = true,
         },
       },
     })
