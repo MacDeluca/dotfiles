4a210324f9c0dae0f9f6a5f9c200ee29e823bb7e
Mac Deluca <Mac.Deluca@quartech.com>
2025-12-19 13:10:01 -0800

    fix: updated nvim config + wezterm
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,16 @@
--- brew install --cask cognominal/neovim-nightly.fork/neovim-nightly
+-- Install nightly: brew install --cask cognominal/neovim-nightly.fork/neovim-nightly
+-- LSP Configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+--
+-- Wezterm: Alt + Enter -> Toggle Fullscreen
+-- :checkhealth vim.lsp
+-- :Undotree
 
+-- Basic Settings
 vim.g.mapleader = vim.keycode("<Space>") -- Set <Space> as the leader key (used for custom shortcuts)
 vim.opt.relativenumber = true -- Show relative line numbers for easier navigation
-vim.opt.number = true
+vim.opt.number = true -- Show absolute line numbers
 vim.opt.shiftwidth = 2 -- Indent by 2 spaces when shifting text
-vim.opt.tabstop = 2
+vim.opt.tabstop = 2 -- Number of spaces a <Tab> counts for
 vim.opt.smartcase = true -- Search is case-insensitive unless uppercase letters are used
 vim.opt.ignorecase = true -- Ignore case in search patterns by default
 vim.opt.wrap = false -- Disable line wrapping (keep long lines on one line)
@@ -15,125 +21,124 @@ vim.opt.splitright = true -- Open new vertical splits to the right
 vim.opt.splitbelow = true -- Open new horizontal splits below
 vim.opt.clipboard = "unnamedplus" -- Use the system clipboard for all yank/paste operations
 vim.opt.winborder = "single" -- Use a single-line border for floating windows
-vim.opt.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.laststatus = 3
+vim.opt.undofile = true -- Enable persistent undo across sessions
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Directory to store undo history files
+vim.opt.laststatus = 3 -- Always show a single global statusline
 
+-- Package Management
 local packages = {
-	{ src = "https://tpope.io/vim/fugitive.git", name = "fugitive" },
-	{ src = "https://github.com/catppuccin/nvim.git", name = "catppuccin" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons", name = "nvim-web-devicons" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter.git", version = "master", name = "nvim-treesitter" },
-	{ src = "https://github.com/stevearc/oil.nvim.git", name = "oil" },
-	{ src = "https://github.com/christoomey/vim-tmux-navigator.git", name = "vim-tmux-navigator" },
-	{ src = "https://github.com/folke/snacks.nvim.git", name = "snacks" },
-	{ src = "https://github.com/saghen/blink.cmp", name = "blink" },
-	{ src = "https://github.com/neovim/nvim-lspconfig", name = "nvim-lspconfig" },
-	{ src = "https://github.com/stevearc/conform.nvim", name = "conform" },
-	{ src = "https://github.com/zbirenbaum/copilot.lua", name = "copilot" },
+  { src = "https://tpope.io/vim/fugitive.git", name = "fugitive" },
+  { src = "https://github.com/catppuccin/nvim.git", name = "catppuccin" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons", name = "nvim-web-devicons" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter.git", version = "main", name = "nvim-treesitter" },
+  { src = "https://github.com/stevearc/oil.nvim.git", name = "oil" },
+  { src = "https://github.com/christoomey/vim-tmux-navigator.git", name = "vim-tmux-navigator" },
+  { src = "https://github.com/folke/snacks.nvim.git", name = "snacks" },
+  { src = "https://github.com/saghen/blink.cmp", name = "blink" },
+  { src = "https://github.com/neovim/nvim-lspconfig", name = "nvim-lspconfig" },
+  { src = "https://github.com/stevearc/conform.nvim", name = "conform" },
+  { src = "https://github.com/zbirenbaum/copilot.lua", name = "copilot" },
 }
-
 vim.pack.add(packages)
-
 vim.cmd.packadd("nvim.undotree") -- :Undotree
 
+local function hl(hl_group, text) return "%#" .. hl_group .. "#" .. text .. "%#StatusLine#" end
+
 function StatusLine()
-	local errors = #vim.diagnostic.get(vim.api.nvim_get_current_buf(), { severity = vim.diagnostic.severity.ERROR })
-	local warnings = #vim.diagnostic.get(vim.api.nvim_get_current_buf(), { severity = vim.diagnostic.severity.WARN })
-	local diagnostics = errors + warnings > 0 and (" E" .. errors .. " W" .. warnings) or ""
-	return " %f " .. diagnostics .. " %m %=  " .. vim.fn.FugitiveHead() .. " %l:%c "
+  local num_errors = #vim.diagnostic.get(vim.api.nvim_get_current_buf(), { min = vim.diagnostic.severity.ERROR })
+  local err = num_errors > 0 and (" " .. num_errors) or ""
+  return " %f %m " .. hl("SLError", err) .. " %=" .. hl("SLBranch", " " .. vim.fn.FugitiveHead()) .. " %l:%c "
 end
 
-vim.opt.statusline = [[%!luaeval('StatusLine()')]]
+vim.opt.statusline = [[%!luaeval("StatusLine()")]]
 
 local palette = require("catppuccin.palettes").get_palette("mocha")
 
 require("catppuccin").setup({
-	float = {
-		transparent = true,
-		solid = false,
-	},
+  float = {
+    transparent = true,
+    solid = false,
+  },
 })
 vim.cmd.colorscheme("catppuccin")
-
+vim.api.nvim_set_hl(0, "SLBranch", { fg = palette.green, bold = true })
+vim.api.nvim_set_hl(0, "SLError", { fg = palette.red })
 vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = palette.surface0 })
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = palette.mauve })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = palette.lavender })
 
 require("blink.cmp").setup({
-	completion = {
-		menu = { border = "none" },
-	},
-	fuzzy = {
-		prebuilt_binaries = {
-			force_version = "v1.7.0",
-		},
-	},
+  completion = {
+    menu = { border = "none" },
+  },
+  fuzzy = {
+    prebuilt_binaries = {
+      force_version = "v1.7.0",
+    },
+  },
 })
 
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "typescript", "javascript", "tsx", "json", "html", "css", "bash", "markdown", "jsdoc" },
-	sync_install = true,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+local languages = { "lua", "typescript", "tsx", "jsx", "json", "html", "css", "bash", "markdown", "jsdoc", "yaml" }
+require("nvim-treesitter").install(languages)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function() pcall(vim.treesitter.start) end,
 })
 
 require("oil").setup({
-	view_options = { show_hidden = true },
-	float = { max_width = 0.6, max_height = 0.8 },
-	keymaps = {
-		["<C-v>"] = "actions.select_vsplit",
-		["<C-x>"] = "actions.select_split",
-		["<CR>"] = "actions.select",
-		["-"] = "actions.parent",
-	},
-	use_default_keymaps = false,
+  view_options = { show_hidden = true },
+  float = { max_width = 0.6, max_height = 0.8 },
+  keymaps = {
+    ["<C-v>"] = "actions.select_vsplit",
+    ["<C-x>"] = "actions.select_split",
+    ["<CR>"] = "actions.select",
+    ["-"] = "actions.parent",
+  },
+  use_default_keymaps = false,
 })
 
 require("snacks").setup({
-	styles = { float = { backdrop = 100 } },
-	bigfile = { enabled = true },
-	picker = { enabled = true },
+  styles = { float = { backdrop = 100 } },
+  bigfile = { enabled = true },
+  picker = { enabled = true },
 })
 
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		typescript = { "prettierd" },
-		typescriptreact = { "prettierd" },
-		cs = { "csharpier" },
-		kt = {},
-	},
-	default_format_opts = {
-		lsp_format = "format",
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
-	notify_on_error = true,
-	notify_no_formatters = true,
+  formatters_by_ft = {
+    lua = { "stylua" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    cs = { "csharpier" },
+    kt = {},
+  },
+  default_format_opts = {
+    lsp_format = "format",
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+  notify_on_error = true,
+  notify_no_formatters = true,
 })
 
 require("copilot").setup({
-	copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v22.19.0/bin/node",
-	suggestion = {
-		enabled = true,
-		auto_trigger = true,
-		debounce = 25,
-		keymap = {
-			accept = "<C-g>",
-			next = "<M-n>",
-			prev = "<M-p>",
-		},
-	},
-	filetypes = {
-		yaml = true,
-		markdown = true,
-	},
+  copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v22.19.0/bin/node",
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 25,
+    keymap = {
+      accept = "<C-g>",
+      next = "<M-n>",
+      prev = "<M-p>",
+    },
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+  },
 })
 
 -- stylua: ignore start
@@ -147,67 +152,84 @@ vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
 vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
 vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
 vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
-vim.keymap.set('n', '<leader>j', '<cmd>diffget //3<CR>')
-vim.keymap.set('n', '<leader>f', '<cmd>diffget //2<CR>')
+vim.keymap.set("n", "<leader>j", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "<leader>f", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gc", ":Git commit -n -s -m ''<Left>")
 vim.keymap.set("n", "<leader>gb", function() require("snacks").picker.git_branches() end)
 vim.keymap.set("n", "<leader>ph", function() require("snacks").picker.recent() end)
 vim.keymap.set("n", "<leader>pf", function() require("snacks").picker.smart({ hidden = true }) end)
 vim.keymap.set("n", "<leader>ps", function() require("snacks").picker.grep({ hidden = true }) end)
+vim.keymap.set("n", "<leader>pb", function() require("snacks").picker.buffers() end)
 vim.keymap.set("n", "<leader>pw", function() require("snacks").picker.grep_word() end)
 vim.keymap.set("n", "gd", function() require("snacks").picker.lsp_definitions() end)
-vim.keymap.set('n', 'gv', ':vsplit | lua Snacks.picker.lsp_definitions()<CR>', { noremap = true, silent = true })
-vim.keymap.set('v', 'y', 'ygv<Esc>', { desc = 'better cusor position on yank' })
-vim.keymap.set('n', 'N', 'Nzzzv')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set("n", "gv", ":vsplit | lua Snacks.picker.lsp_definitions()<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "y", "ygv<Esc>", { desc = "better cusor position on yank" })
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
 -- stylua: ignore end
 
--- :checkhealth vim.lsp
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-vim.lsp.config("*", { capabilities = capabilities })
+vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities() })
 
 vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			runtime = { version = "LuaJIT" }, -- Or "5.1", "5.2", "5.3", "5.4" },
-			diagnostics = { globals = { "vim" } },
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-					vim.fn.stdpath("config"),
-				},
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" }, -- Or "5.1", "5.2", "5.3", "5.4" },
+      diagnostics = { globals = { "vim" } },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          vim.fn.stdpath("config"),
+        },
+      },
+    },
+  },
 })
 
 vim.lsp.config("ts_ls", {
-	settings = {
-		diagnostics = {
-			ignoredCodes = { 80004 }, -- Disable the JSDoc type hint
-		},
-	},
+  settings = {
+    diagnostics = {
+      ignoredCodes = { 80004 },
+    },
+  },
 })
 
 vim.lsp.config("eslint", {
-	settings = {
-		nodePath = vim.fn.getcwd() .. "/node_modules",
-	},
+  settings = {
+    nodePath = vim.fn.getcwd() .. "/node_modules",
+  },
 })
 
 vim.lsp.enable({ "lua_ls", "ts_ls", "eslint", "sourcekit" })
 
 vim.api.nvim_create_user_command("UpdatePackages", function()
-	local package_names = {}
-	for index, pkg in ipairs(packages) do
-		package_names[index] = pkg.name
-	end
-	vim.pack.update(package_names)
-	vim.cmd("TSUpdate") -- Update treesitter
+  local package_names = {}
+  for index, pkg in ipairs(packages) do
+    package_names[index] = pkg.name
+  end
+  vim.pack.update(package_names)
+  vim.cmd("TSUpdate") -- Update treesitter
 end, {})
+
+vim.api.nvim_create_user_command(
+  "Config",
+  function() vim.cmd("vsplit " .. vim.fn.stdpath("config") .. "/init.lua") end,
+  {}
+)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightYankGroup", { clear = true }),
+  desc = "quickly highlights yanked text",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 50,
+    })
+  end,
+})
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,49 +1,49 @@
+-- Alt + Enter -> Toggle Fullscreen
+
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.default_cursor_style = 'SteadyBar'
+config.default_cursor_style = "SteadyBar"
 
 -- This is where you actually apply your config choices.
 -- config.default_domain = 'WSL:Ubuntu'
 
-config.default_cwd = '~'
+config.default_cwd = "~"
 
 -- CTRL + SHIFT + L and type wezterm.gui.enumerate_gpus()
 
-config.front_end = 'WebGpu'
+config.front_end = "WebGpu"
 config.webgpu_preferred_adapter = wezterm.gui.enumerate_gpus()[1]
-
--- -- config.webgpu_preferred_adapter = {
--- --   backend = 'Dx12',
--- --   device = 10400,
--- --   device_type = 'DiscreteGpu',
--- --   name = 'NVIDIA GeForce RTX 4060 Laptop GPU',
--- --   vendor = 4318
--- -- }
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 14
-config.color_scheme = 'Catppuccin Mocha'
+config.font_size = 16
+config.color_scheme = "Catppuccin Mocha"
 
 -- remove all padding from editor
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
---config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE|RESIZE"
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.webgpu_power_preference = 'HighPerformance'
+config.webgpu_power_preference = "HighPerformance"
+
+config.window_close_confirmation = "NeverPrompt"
+
+config.native_macos_fullscreen_mode = true
+config.macos_window_background_blur = 0
+
 -- Finally, return the configuration to wezterm:
 return config
